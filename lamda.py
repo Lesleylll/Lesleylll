a109b71032ef8e501ea4f38af6f69df3fbf36e84
Quanfeng Yuwen <104770228+Lesleylll@users.noreply.github.com>
2022-08-11 19:32:07 +0800

    UCBSC61A

--- lamda.py
+++ lamda.py
@@ -1,3 +1,16 @@
 """lambda"""
-g = lambda x: x * x
-g(9)
+def composel(f,g):
+    def composed(x):
+        return f(g(x))
+    return composed
+    #return lambda x: f(g(x))
+
+def square(x):
+    return x * x
+
+def add_one(x):
+    return x + 1
+
+h = composel(square, add_one)
+#h = composel(lambda x : x * x, lambda y : y + 1)
+result = h(12) 
